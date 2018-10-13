ba6ad94873da89279e0fa2e3c01d01738dea4ec7
Michael Hanke <michael.hanke@gmail.com>
2018-10-13 11:17:41 +0200

    Rough sketch of the submission template

--- datalad_htcondor/htcprepare.py
+++ datalad_htcondor/htcprepare.py
@@ -27,6 +27,45 @@ from datalad.distribution.dataset import datasetmethod
 lgr = logging.getLogger('datalad.htcondor.htcprepare')
 
 
+submission_template = """\
+Universe     = vanilla
+Executable   = {executable}
+
+# true or false
++WantIOProxy = {ioproxy_flag}
+
+#+SingularityImage = "sing.simg"
+{singularityimg_spec}
+
+# all values for PRE and POST script MUST be quoted!
+# name in execute dir on execute node
++PreCmd       = "{preflight_script}"
++PreArguments = "{preflight_script_args}"
++PreEnvironment = "{preflight_script_env}"
++PostCmd       = "{postflight_script}"
++PostArguments = "{postflight_script_args}"
++PostEnvironment = "{postflight_script_env}"
+
+# transfer and execution setup
+run_as_owner = {run_as_owner_flag}
+should_transfer_files = {transfer_files_mode}
+when_to_transfer_output = {transfer_output_mode}
+
+# each job has its own dir, to receive the outputs back
+initial_dir = job_\$(Process)
+
+# paths must be relative to initial dir
+transfer_input_files = {transfer_files_list}
+
+# paths are relative to a job's initial dir
+Error   = logs/err
+# TODO support this case
+#Input   = logs/in
+Output  = logs/out
+Log     = logs/log
+"""
+
+
 @build_doc
 class HTCPrepare(Interface):
     """TODO
@@ -37,6 +76,10 @@ class HTCPrepare(Interface):
         singularity=Parameter(
             args=("--singularity",),
             doc="""ADDME"""),
+        jobcfg=Parameter(
+            args=("--jobcfg",),
+            doc="""name of pre-crafted job configuration that is used to
+            the tailor the HTCondor setup."""),
     )
 
     @staticmethod
@@ -51,8 +94,6 @@ class HTCPrepare(Interface):
             explicit=False,
             message=None,
             sidecar=None,
-            singularity=None):
-        if rerun:
-            raise
-
+            singularity=None,
+            jobcfg=None):
         pass
